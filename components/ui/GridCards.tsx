93caca0b94de2033ee3bab4084aea946c8a50ef3
michaelyosimar43 <michaelyosimar43@gmail.com>
2024-10-07 20:50:48 +0000

    muchas cosas

--- components/ui/GridCards.tsx
+++ components/ui/GridCards.tsx
@@ -1,25 +1,30 @@
 interface GridCardsProps {
-  imageSrc: string;
-  title: string;
-  description: string;
+
+    list: {
+        imageSrc: string;
+        title: string;
+        description: string;
+    }[],
+    color: "bg-blue-400" | "bg-red-400" | "bg-green-400"
 }
 
 
-const GridCards: React.FC<GridCardsProps> = ({ imageSrc, title, description }) => {
-  return (
-    <div className="grid grid-cols-2 auto-rows-auto bg-blue-400 p-3 border-l-rose-600">
-      <div className="bg-white p-4">
-        <img src={imageSrc} alt={title} />
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-      <div className="bg-white p-4">
-        <img src={imageSrc} alt={title} />
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+const GridCards: React.FC<GridCardsProps> = ({ list, color }) => {
+    return (
+        <div className={"grid grid-cols-2 auto-rows-auto text-center p-3 border-l-rose-600 justify-center items-center gap-4  place-items-center pt4" + color}>
+            {
+                list.map(({ title, description, imageSrc }) => (
+                    <div className="bg-white p-4 mx-auto">
+                        <img src={imageSrc} alt={title} className="w-1/2 y-1/2 mx-auto" />
+                        <h3>{title}</h3>
+                        <p>{description}</p>
+                    </div>))
+
+
+            }
+
+        </div>
+    );
 };
 
 export default GridCards;
--- pages/kids.tsx
+++ pages/kids.tsx
@@ -4,6 +4,8 @@ import NIÑO from "@/imgs/children-playing-with-lego-playing-room.jpg"
 import NOVACH from "@/imgs/lasos.png"
 import lego from "@/imgs/Rectangle.png"
 import DescriptionCurse, { descriptionCurseProps } from "@/components/ui/descriptionCurse";
+import GridsCards from "@/components/ui/GridCards";
+import Footer from "@/components/ui/Footer";
 
 
 
@@ -12,15 +14,25 @@ import DescriptionCurse, { descriptionCurseProps } from "@/components/ui/descrip
 // // Suggested code may be subject to a license. Learn more: ~LicenseLog:2570757612.
 // // Suggested code may be subject to a license. Learn more: ~LicenseLog:3826708466.
 let idata: descriptionCurseProps = {
-   
-        description: "Lorem ipsum dolor sit amet,e mags nisi ut aliquip ex ea commodu fugiat nulla pariatur cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        list: ["Learn the basic of coding", "Build your first game", "Have fun learning"],
-        src: lego.src
-    
+
+    description: "Lorem ipsum dolor sit amet,e mags nisi ut aliquip ex ea commodu fugiat nulla pariatur cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    list: ["Learn the basic of coding", "Build your first game", "Have fun learning"],
+    src: lego.src
+
 
 }
 
-
+let gridcarddata = [
+    {
+        imageSrc: lego.src,
+        title: "un titulo ",
+        description: "una descricption "
+    }, {
+        imageSrc: lego.src,
+        title: "un titulo ",
+        description: "una descricption "
+    }
+]
 
 
 export default function Kids() {
@@ -29,7 +41,8 @@ export default function Kids() {
             <AppMenu />
             <HeroImage src={NIÑO.src} alt={"hola"} logo={NOVACH.src} />
             <DescriptionCurse {...idata} />
-
+            <GridsCards color="bg-blue-400" list={gridcarddata} />
+            <Footer />
         </div>
     );
 }
