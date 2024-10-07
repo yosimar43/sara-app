bfadb42d0efe9e5eda483661c5aabf0c1093c0ab
michaelyosimar43 <michaelyosimar43@gmail.com>
2024-10-07 22:19:05 +0000

    cosas

--- components/ui/CardsKids.tsx
+++ components/ui/CardsKids.tsx
@@ -1,18 +1,18 @@
 import Image from 'next/image';
 import React from 'react';
 
-interface Props {
+export interface PropsCard {
     title: String,
     description: String,
     ageRange: String,
     image: any,
+    color:"bg-blue-300"|"bg-red-300"|"bg-yellow-300"
 }
-const CardsKids: React.FC<Props> = ({ ageRange, description, image, title }) => {
+const CardsKids: React.FC<PropsCard> = ({ ageRange, description, image, title ,color}) => {
     return (
-        <section className="bg-red-100 relative text-center h-[50vh]">
-            <h3 className="w-full text-white bg-blue-900  px-4 mx-auto  min-h-7 h-[20%] ">CATEGORÍAS</h3>
-            <div className="container mx-auto h-[90%]  flex-grow">
-                <div className="flex flex-col md:flex-row bg-white  shadow-md overflow-hidden">
+        <section className=" relative text-center h-[50vh] bg-red-500 flex ">
+            <div className="  flex-grow">
+                <div className="flex flex-col md:flex-row bg-white  h-full shadow-md overflow-hidden">
                     <div className="relative w-[45%] bg-cover bg-center" style={{ backgroundImage: `url(${image})` }}>
                         <div className="absolute inset-0 bg-blue-900 bg-opacity-50 flex items-center justify-center">
                             <Image
@@ -26,7 +26,7 @@ const CardsKids: React.FC<Props> = ({ ageRange, description, image, title }) => 
 
                     </div>
 
-                    <div className="w-full flex-grow p-6 bg-blue-200 flex flex-col  items-center justify-evenly">
+                    <div className={"w-full flex-grow p-6  flex flex-col  items-center justify-evenly "+color}>
                         <h3 className="text-3xl font-bold text-black">{title}</h3>
                         <p className="text-black mb-4">{ageRange}</p>
                         <p className="text-black mb-6">
